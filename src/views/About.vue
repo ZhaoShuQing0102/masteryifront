68bd854d3ef932457b943ebb561f453afe23d74e
ZhaoShuQing0102 <2391081412@qq.com>
2021-07-13 17:41:04 +0800

    个人中心简易版finish

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,115 +1,124 @@
 <template>
-  <div class="about">
-    <el-container>
-      <el-aside width="30%">
-      <el-col :span="25" style="float: left; margin-top: 5%;margin-left: 75%">
-        <h3 @click="name=1">我的个人中心</h3>
-        <el-menu
-            :uniqueOpened="true"
-            default-active="2"
-            class="el-menu-vertical-demo"
-            background-color="#fff"
-            text-color="#303133">
-          <el-menu-item-group>
-            <el-menu-item index="1" @click="name=2">个人信息</el-menu-item>
-            <el-menu-item index="2" @click="name=3">修改密码</el-menu-item>
-            <el-menu-item index="3" @click="name=4">收货地址</el-menu-item>
-          </el-menu-item-group>
-        </el-menu>
-      </el-col>
-      </el-aside>
-      <el-main>
-      <div class="mshow name1" v-if="name===1" style="margin-top: 2%;">
-        <div class="name1-head" style="height: 30%; width: 100%;">
+    <el-container style="margin-left: 20%;width: 60%">
+        <el-header height="250px">
+            <div> <el-image
+                style="width: 150px; height: 150px;border-radius: 50%; margin-top:50px;margin-left:20%;float: left"
+                :src=user.userImgUrl
+                :fit="fit">
+            </el-image>
+            </div>
+            <div style="margin-top: 50px;float: left;margin-left: 10%">
+                <h1>{{user.userName}}</h1>
+                <h3 style="margin-top: 50px">-_- 欢迎您！！！</h3>
+            </div>
+        </el-header>
+        <el-main>
+            <div style="margin-top: 20px;margin-left: 20%;text-align: left">
+                <h3>手机号：{{user.phoneNum}}</h3>
+            </div>
+            <div style="margin-top: 50px;margin-left: 20%;text-align: left">
+                <h3>地址：{{user.address}}</h3>
+            </div>
+            <div style="margin-top: 50px;margin-left: 20%;text-align: left">
+                <h3>消费：{{user.consumption}}</h3>
+            </div>
+            <div style="margin-top: 50px;margin-left: 20%;float:left">
+                <el-button type="primary" icon="el-icon-edit" circle @click="open_edit">
+                </el-button>
+                修改个人信息
+            </div>
+            <div style="margin-top: 50px;margin-left: 10%;float:left">
+                <router-link :to="'/order/'">
+                <el-button type="primary" icon="el-icon-edit" circle>
+                </el-button>
+                查看个人订单
+                </router-link>
+            </div>
+        </el-main>
+        <el-main v-if="edit">
+            <el-form ref="form" :model="form" label-width="80px">
+                <el-form-item label="用户名">
+                    <el-input v-model="form.user_name"></el-input>
+                </el-form-item>
+                <el-form-item label="新密码">
+                    <el-input v-model="form.user_pwd"></el-input>
+                </el-form-item>
+                <el-form-item label="新手机号">
+                    <el-input v-model="form.phone_num"></el-input>
+                </el-form-item>
+                <el-form-item label="新地址">
+                    <el-input v-model="form.user_address"></el-input>
+                </el-form-item>
+                <el-form-item label="新头像">
+                    <el-input v-model="form.user_img"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="change_user_info">立即更改</el-button>
+                    <el-button type="primary" @click="edit=false">取消</el-button>
+                </el-form-item>
+            </el-form>
+        </el-main>
+    </el-container>
 
-        </div>
-        <el-divider></el-divider>
-        <div class="first" style="float: left;margin-top: 10%;width: 100%">
-        <div class="my-pay" style="float: left; margin-left: 10%;">
-          <h4>待支付的订单： 0</h4>
-        </div>
-        <div class="look-my-pay" style=" margin-left: 60%;">
-          <h4>查看待支付的订单></h4>
-        </div>
-        </div>
-        <div class="second" style="float: left;margin-top: 10%;width: 100%">
-          <div class="my-goods" style="float: left; margin-left: 10%;">
-            <h4>待收货的订单： 0</h4>
-          </div>
-          <div class="look-my-goods" style=" margin-left: 60%;">
-            <h4>查看待收货的订单></h4>
-          </div>
-        </div>
-        <div class="third" style="float: left;margin-top: 10%;width: 100%">
-          <div class="my-comment" style="float: left; margin-left: 10%;">
-            <h4>待评价的订单： 0</h4>
-          </div>
-          <div class="look-my-comment" style=" margin-left: 60%;">
-            <h4>查看待评价的订单></h4>
-          </div>
-        </div>
-        <div class="forth" style="float: left;margin-top: 10%;width: 100%">
-          <div class="my-comment" style="float: left; margin-left: 10%;">
-            <h4>待收藏的订单： 0</h4>
-          </div>
-          <div class="look-my-comment" style=" margin-left: 60%;">
-            <h4>查看待收藏的订单></h4>
-          </div>
-        </div>
-      </div>
-      <div class="mshow name2" v-if="name===2" style="margin-top: 2%;border-top:1px solid #FFFFFF;border-left:1px solid #FFFFFF;">
-        <div class="name2-head" style="margin-top:5%;float: left;margin-left: 5%;">
-           <h3>个人信息</h3>
-        </div>
-        <div class="headshot" style="float:left;margin-top: 10%;margin-left:15%;clear: both">
-             <h4>头像:</h4>
-        </div>
-        <div class="nick-name" style="float:left;margin-top: 10%;margin-left:15%;clear: both">
-             <h4>昵称:jkl</h4>
-        </div>
-        <div class="sex" style="float:left;margin-top: 10%;margin-left:15%;clear: both">
-             <h4>性别:jkl</h4>
-        </div>
-        <div class="signature" style="float:left;margin-top: 10%;margin-left:15%;clear: both">
-          <h4>个性签名:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</h4>
-        </div>
-        <div class="phone-num" style="float:left;margin-top: 10%;margin-left:15%;clear: both">
-          <h4>手机号:xxxxxxxx</h4>
-        </div>
-        <div class="edit" style="float:left;margin-top: 10%;margin-left:50%;clear: both">
-        <el-button size="medium" type="warning">编辑</el-button>
-        </div>
-      </div>
-    <div v-else-if="name==3">
-      789
-    </div>
-    <div class="mshow name2" v-else-if="name==4" style="margin-top: 2%;border-top:1px solid #FFFFFF;border-left:1px solid #FFFFFF;">
-        <div class="name3-head" style="width: 100%;height: 15%;">
-
-        </div>
-      <el-divider></el-divider>
-      <div class="name3-body" style="width: 100%;height: 35%;">
-
-      </div>
-      <el-divider></el-divider>
-    </div>
-  </el-main>
-  </el-container>
-  </div>
 </template>
+
 <script>
-   export default {
-     data() {
-       return {
-         name: 1
-       }
-     }
-   }
+import axios from "axios";
+import Qs from "qs";
+
+export default {
+    data() {
+        return {
+            userId:2,
+            name: 1,
+            user:'',
+            edit:false,
+            form:{
+                user_name: '',
+                user_pwd: '',
+                phone_num: '',
+                user_address: '',
+                user_img:''
+            }
+        }
+    },
+    mounted() {
+        axios.post("http://localhost:8083/user/getuser",Qs.stringify({userId:this.userId}))
+            .then(res => {
+                this.user = res.data.data;
+                console.log(this.user);
+            })
+    },
+    methods:{
+        open_edit() {
+            this.edit=true;
+        },
+        change_user_info(){
+            axios.post("http://localhost:8083/user/changeuserinfo",
+                Qs.stringify({userId:this.userId,
+                                userName:this.form.user_name,
+                                userPwd:this.form.user_pwd,
+                                phoneNum:this.form.phone_num,
+                                userAddress:this.form.user_address,
+                                userImgUrl:this.form.user_img}))
+            .then(res => {
+                if(res.data.code==="200"){
+                    this.$message({
+                        type:"success",
+                        message:"成功更改",
+                        offset: 150
+                    });
+                    this.edit = false;
+                }
+            })
+        }
+    }
+}
 </script>
 <style>
-   .mshow{
-     width: 800px;
-     height: 700px;
-     background: #FFFFFF;
-   }
+.mshow {
+    width: 800px;
+    height: 700px;
+    background: #FFFFFF;
+}
 </style>
